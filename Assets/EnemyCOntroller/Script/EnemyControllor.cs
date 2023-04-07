f3052b656e5592a2782f13f1e744465cf9deadd3
YanwenWang1125 <ywan3625@uwo.ca>
2023-04-06 21:04:24 -0400

    shopping updates

---
+++ Assets/EnemyCOntroller/Script/EnemyControllor.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class EnemyControllor : MonoBehaviour
+{
+    public Transform target; 
+    public GameObject bulletPrefab; 
+    public float bulletSpeed = 10f; 
+    public float fireInterval = 2f; 
+    public float flySpeed = 10f;
+    public float turnSpeed = 5f;
+    public float changeInterval = 5f; 
+    public float changeDistance = 10f; 
+
+    private Rigidbody rb;
+    private Vector3 targetDirection; 
+    private float fireTimer; 
+    private float changeTimer; 
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+
+        
+        fireTimer = fireInterval;
+        changeTimer = changeInterval;
+    }
+
+    void FixedUpdate()
+    {
+        transform.LookAt(target);
+       
+        // 发射炮弹
+        fireTimer -= Time.fixedDeltaTime;
+        if (fireTimer <= 0)
+        {
+            fireTimer = fireInterval;
+            Fire();
+        }
+
+        // 改变方向
+        changeTimer -= Time.fixedDeltaTime;
+        if (changeTimer <= 0)
+        {
+            changeTimer = changeInterval;
+            targetDirection = Random.insideUnitSphere;
+            targetDirection.y = 0;
+            targetDirection = targetDirection.normalized;
+        }
+
+        // 转向目标方向
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, targetRotation, turnSpeed * Time.fixedDeltaTime));
+
+        // 移动
+        Vector3 direction = rb.rotation * Vector3.forward;
+        rb.velocity = direction * flySpeed;
+    }
+
+    void Fire()
+    {
+        // 创建炮弹
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+
+        // 将炮弹向玩家发射
+        Vector3 direction = (target.position - transform.position).normalized;
+        bullet.GetComponent<Rigidbody>().velocity = direction * bulletSpeed;
+    }
+}
---
+++ Assets/EnemyCOntroller/Script/mouse.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class mouse: MonoBehaviour
+{
+    public GUIStyle guiStyle; // GUI样式
+    public Texture2D cursorTexture; // 光标纹理
+    public Vector2 cursorHotspot; // 光标热点
+
+    void Start()
+    {
+        // 隐藏系统光标
+        Cursor.visible = false;
+    }
+
+    void OnGUI()
+    {
+        // 获取鼠标位置
+        Vector2 mousePosition = Event.current.mousePosition;
+        mousePosition.y = Screen.height - mousePosition.y; // 将y坐标转换为屏幕坐标系
+
+        // 显示鼠标位置
+        GUI.Label(new Rect(mousePosition.x + 10f, mousePosition.y - 10f, 100f, 20f), "Mouse: (" + mousePosition.x + ", " + mousePosition.y + ")", guiStyle);
+
+        // 显示移动的点
+        GUI.DrawTexture(new Rect(mousePosition.x - cursorHotspot.x, mousePosition.y - cursorHotspot.y, cursorTexture.width, cursorTexture.height), cursorTexture);
+    }
+
+    void OnDestroy()
+    {
+        // 恢复系统光标
+        Cursor.visible = true;
+    }
+}
